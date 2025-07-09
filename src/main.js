527fc54b8894b5ffa4e3bfd28dd82b015278e8ec
AL <alcoran026@gmail.com>
2025-07-09 15:08:38 +0800

    [vite] Setup the main banner of the page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import setFooter from './components/footer.js';
+import setMainBanner from './components/mainBanner.js';
 import setNavigation from './components/navigation.js';
 
 document.querySelector('#app').innerHTML = `
    <nav id="navigation" class="nav-container"></nav>
+   <div id="main-banner" class="main-banner-container"></div>
    <footer id="footer" class="footer-container"></footer>
 `;
 
 setNavigation(document.getElementById('navigation'));
+setMainBanner(document.getElementById('main-banner'));
 setFooter(document.getElementById('footer'));
 
