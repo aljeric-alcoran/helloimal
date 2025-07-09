614ec9c3ebc802d2bfaebc0725b9c3f9e2bf7df5
AL <alcoran026@gmail.com>
2025-07-09 15:22:46 +0800

    [vite] Add biography section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import setBiography from './components/biography.js';
 import setFooter from './components/footer.js';
 import setMainBanner from './components/mainBanner.js';
 import setNavigation from './components/navigation.js';
@@ -5,10 +6,12 @@ import setNavigation from './components/navigation.js';
 document.querySelector('#app').innerHTML = `
    <nav id="navigation" class="nav-container"></nav>
    <div id="main-banner" class="main-banner-container"></div>
+   <div id="biography" class="biography-container"></div>
    <footer id="footer" class="footer-container"></footer>
 `;
 
 setNavigation(document.getElementById('navigation'));
 setMainBanner(document.getElementById('main-banner'));
+setBiography(document.getElementById('biography'));
 setFooter(document.getElementById('footer'));
 
