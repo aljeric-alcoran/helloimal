d45e6a5335d3c0a87b258441329072cd558662cd
AL <alcoran026@gmail.com>
2025-07-10 23:27:56 +0800

    [contact] Added contact form

---
+++ src/components/contact.js
@@ -0,0 +1,27 @@
+const template = `
+   <div class="max-w-screen-xl mx-auto p-8 md:py-16 text-gray-900 dark:text-white">
+      <h1 class="text-2xl font-semibold">Contact Me</h1>
+
+      <form class="max-w-sm mt-10">
+         <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="johndoe@email.com" required />
+         </div>
+         <div class="mb-5">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+            <input type="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+         </div>
+         <div class="mb-5">
+            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your message..." required></textarea>
+         </div>
+         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send message</button>
+      </form>
+   </div>
+`;
+
+const setContactSection = (element) => {
+   element.innerHTML = template;
+}
+
+export default setContactSection;
