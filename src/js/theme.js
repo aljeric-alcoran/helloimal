156d42134fd021c7c685b1b4a1aa38f72a153f09
AL <alcoran026@gmail.com>
2025-07-11 15:54:46 +0800

    [contact] Remove unused and undefined function

--- src/js/theme.js
+++ src/js/theme.js
@@ -23,7 +23,6 @@ const setTheme = () => {
       document.documentElement.classList.toggle('dark', newTheme === 'dark');
    
       switchThemIcon(newTheme);
-      initializeReCaptcha(newTheme);
       // Save to localStorage
       localStorage.setItem('theme', newTheme);
    }
