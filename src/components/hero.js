baa6c390b8abe5a3624ed3b3facb7d9cc025a324
AL <alcoran026@gmail.com>
2025-07-14 22:24:55 +0800

    [cta] Refactor file names.

--- src/components/hero.js
+++ src/components/hero.js
@@ -14,7 +14,7 @@ const template = `
             Download CV
          </button>
 
-         <button type="button" class="cursor-pointer bg-[#3150CE]/10 hover:bg-[#3150CE]/20 text-[#3150CE] border border-[#3150CE] hover:border-[#3150CE]/90 focus:ring-2 focus:outline-none focus:ring-[#3150CE]/50 font-medium rounded-sm text-sm px-4 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3150CE]/55 me-2 mb-2 gap-1">
+         <button type="button" class="cursor-pointer text-[#3150CE] bg-[#3150CE]/10 hover:bg-[#3150CE]/15 border border-[#3150CE] hover:border-[#3150CE]/90 focus:ring-2 focus:outline-none focus:ring-[#3150CE]/50 font-medium rounded-sm text-sm px-4 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3150CE]/55 me-2 mb-2 gap-1">
             <svg class="w-5 h-5 text-[#3150CE]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
             </svg>                                     
@@ -24,8 +24,8 @@ const template = `
    </div>
 `;
 
-const setMainBannerSection = (element) => {
+const setHeroSection = (element) => {
    element.innerHTML = template;
 };
 
-export default setMainBannerSection;
+export default setHeroSection;
