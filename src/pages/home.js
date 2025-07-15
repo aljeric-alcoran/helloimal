9f1e380499b0e8baa828dcfe31946991cd879448
AL <alcoran026@gmail.com>
2025-07-15 22:59:17 +0800

    [router] Added pages to load it using router

---
+++ src/pages/home.js
@@ -0,0 +1,22 @@
+const template = `
+   <div id="main-banner" class="main-banner-container"></div>
+   <div id="biography" class="bg-gray-100 dark:bg-gray-800"></div>
+   <div id="technical-skills" class="bg-white dark:bg-gray-900"></div>
+   <div id="contact" class="bg-gray-100 dark:bg-gray-800"></div>
+`
+
+import setBiographySection from '../components/biography.js';
+import setContactSection from '../components/contact.js';
+import setHeroSection from '../components/hero.js';
+import setTechnicalSkillsSection from '../components/technicalSkills.js';
+
+const setHomePage = (container) => {
+   container.innerHTML = template;
+
+   setHeroSection(document.getElementById('main-banner'));
+   setBiographySection(document.getElementById('biography'));
+   setTechnicalSkillsSection(document.getElementById('technical-skills'));
+   setContactSection(document.getElementById('contact'));
+}
+
+export default setHomePage;
