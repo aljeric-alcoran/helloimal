74e21006d91081a08613576a3eaed6b7fb6c528d
AL <alcoran026@gmail.com>
2025-08-05 00:54:29 +0800

    [journals] Fix buttons prev/next in image sliders

--- src/components/modal/imageSlider.js
+++ src/components/modal/imageSlider.js
@@ -1,10 +1,16 @@
 const template = `
    <div>
-      <button id="closeModal" class="absolute cursor-pointer top-8 right-12 text-white text-2xl hover:text-gray-300">⛌</button>
-      <div class="w-full max-w-5xl px-4 flex items-center justify-center">
-         <button id="prevImage" class="absolute cursor-pointer left-8 text-white text-5xl px-6 hover:text-gray-300">&lsaquo;</button>
-         <img id="modalImage" src="" alt="Modal Image" class="max-h-[80vh] rounded-sm shadow-lg object-contain mx-auto" />
-         <button id="nextImage" class="absolute cursor-pointer right-8 text-white text-5xl px-6 hover:text-gray-300">&rsaquo;</button>
+      <button id="closeModal" class="absolute cursor-pointer top-4 right-4 md:top-8 md:right-12 text-white text-2xl hover:text-gray-300">⛌</button>
+      <div class="flex items-center justify-between px-4 md:hidden">
+         <button id="prevImage" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-sm text-sm px-4 py-1 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">⮜ Prev</button>
+         <button id="nextImage" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-sm text-sm px-4 py-1 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Next ⮞</button>
+      </div>
+      <div class="w-full max-w-5xl md:px-4 flex items-center justify-center">
+         <button id="prevImage" class="hidden md:block absolute cursor-pointer left-0 md:left-8 text-white text-5xl px-6 hover:text-gray-300">&lsaquo;</button>
+         <div class="w-full h-full min-h-[70vh] flex items-center">
+            <img id="modalImage" src="" alt="Modal Image" class="max-h-[80vh] md:rounded-sm shadow-lg object-contain mx-auto" />
+         </div>
+         <button id="nextImage" class="hidden md:block absolute cursor-pointer right-0 md:right-8 text-white text-5xl px-6 hover:text-gray-300">&rsaquo;</button>
       </div>
    </div>
 `
@@ -15,8 +21,8 @@ const imageSliderModal = (openModalBtn, galleryImages, currentIndex) => {
 
    const modalImage = document.getElementById('modalImage');
    const closeModalBtn = document.getElementById('closeModal');
-   const prevBtn = document.getElementById('prevImage');
-   const nextBtn = document.getElementById('nextImage');
+   const prevBtn = document.querySelectorAll('#prevImage');
+   const nextBtn = document.querySelectorAll('#nextImage');
 
    const updateImage = () => {
       modalImage.src = galleryImages[currentIndex].src;
@@ -33,15 +39,19 @@ const imageSliderModal = (openModalBtn, galleryImages, currentIndex) => {
       document.body.classList.remove('overflow-hidden');
    });
 
-   prevBtn.addEventListener('click', () => {
-      currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
-      updateImage();
-   });
+   prevBtn.forEach(btn => {
+      btn.addEventListener('click', () => {
+         currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
+         updateImage();
+      });
+   })
 
-   nextBtn.addEventListener('click', () => {
-      currentIndex = (currentIndex + 1) % galleryImages.length;
-      updateImage();
-   });
+   nextBtn.forEach(btn => {
+      btn.addEventListener('click', () => {
+         currentIndex = (currentIndex + 1) % galleryImages.length;
+         updateImage();
+      });
+   })
 }
 
 export default imageSliderModal;
